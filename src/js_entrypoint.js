6fa25a8ff921f883f0a229078487bd43288c69db
JQ <jqocon@gmail.com>
2021-05-28 03:34:02 +0100

    chore: config

--- src/js_entrypoint.js
+++ src/js_entrypoint.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+require('dotenv').config();
 require('ts-node').register();
 require('./ts-kraken-repl/repl_cli.ts');
