e5e777641d3d0b45d93d5e55465a6ae8f347728b
JQ <jqocon@gmail.com>
2021-06-09 16:06:29 +0100

    wip: types

--- src/rest/public/helpers/get_ticker_info.ts
+++ src/rest/public/helpers/get_ticker_info.ts
@@ -1,9 +1,26 @@
-import { PriceTicker } from '../../../types/price_ticker'
+import { Ticker } from '../../../types/rest'
 import { publicRESTRequest } from '../public_rest_request'
 
-// 
-// https://www.kraken.com/features/api#get-ticker-info
-// 
+/* https://www.kraken.com/features/api#get-ticker-info
+
+    a = ask array(<price>, <whole lot volume>, <lot volume>),
+    b = bid array(<price>, <whole lot volume>, <lot volume>),
+    c = last trade closed array(<price>, <lot volume>),
+    v = volume array(<today>, <last 24 hours>),
+    p = volume weighted average price array(<today>, <last 24 hours>),
+    t = number of trades array(<today>, <last 24 hours>),
+    l = low array(<today>, <last 24 hours>),
+    h = high array(<today>, <last 24 hours>),
+    o = today's opening price
+*/
+
+export type PriceTicker = {
+    utcTimestamp: number;
+    pair: string;
+    price: string;
+    rawKrakenPayload: Ticker.Result;
+}
+
 export const getTickerInfo = async (pair: string): Promise<PriceTicker> => {
     const result = await publicRESTRequest({ url: 'Ticker', params: { pair }})
     
