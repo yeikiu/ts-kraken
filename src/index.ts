3754e786d67c399f40741974aa2e2108780bc5b7
JQ <jqocon@gmail.com>
2024-08-16 12:56:54 +0100

    chore: exposed helpers

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,6 @@ export { privateRestRequest, PrivateRestHelpers } from './api/rest/private';
 
 export { publicWsSubscription, publicWsRequest } from './api/ws/public/public_ws_client';
 export { privateWsSubscription, privateWsRequest } from './api/ws/private/private_ws_client';
+
+export { getTickersPrices } from './api/rest/public/helpers/get_tickers_prices';
+export { getOpenOrders, getClosedOrders, findClosedOrder, getWsAuthToken } from './api/rest/private/helpers';
