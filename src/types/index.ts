98e3bef7a7e8f1d39df3d8854bcedcf51d6c522d
JQ <jqocon@gmail.com>
2024-10-30 10:37:29 +0000

    fix: types exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
-export type * as PublicRestTypes from './rest/public';
-export type * as PrivateRestTypes from './rest/private';
+export * as PublicRestTypes from './rest/public';
+export * as PrivateRestTypes from './rest/private';
 
-export type * as PublicWsTypes from './ws/public';
-export type * as PrivateWsTypes from './ws/private';
+export * as PublicWsTypes from './ws/public';
+export * as PrivateWsTypes from './ws/private';
