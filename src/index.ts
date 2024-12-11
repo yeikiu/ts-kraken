51798fe7cfb8522c52c98f0cf64705b8fc4fece2
JQ <jqocon@gmail.com>
2024-12-11 20:53:36 +0000

    chore: export ws helper subscriptions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const { parsed, error: loadConfigError } = config();
 if (loadConfigError) {
     console.error({ loadConfigError });
 } else {
-    globalThis.env = {...parsed};
+    globalThis.env = { ...parsed };
 }
 
 /* Types exports */
@@ -15,8 +15,25 @@ export type { PublicRestTypes, PrivateRestTypes, PublicWsTypes, PrivateWsTypes }
 export { publicRestRequest } from './api/rest/public';
 export { privateRestRequest } from './api/rest/private';
 
-export { publicWsSubscription, publicWsRequest } from './api/ws/public/public_ws_client';
-export { privateWsSubscription, privateWsRequest } from './api/ws/private/private_ws_client';
+export { PublicWs } from './api/ws';
+export {
+    publicWsSubscription,
+    publicWsRequest,
+    connected$ as publicWsConnected$,
+    disconnected$ as publicWsDisconnected$,
+    heartbeat$ as publicWsHeartbeat$,
+    status$ as publicWsStatus$,
+} from './api/ws/public/public_ws_client';
+
+export { PrivateWs } from './api/ws';
+export {
+    privateWsSubscription,
+    privateWsRequest,
+    connected$ as privateWsConnected$,
+    disconnected$ as privateWsDisconnected$,
+    heartbeat$ as privateWsHeartbeat$,
+    status$ as privateWsStatus$,
+} from './api/ws/private/private_ws_client';
 
 export { getTickersPrices } from './api/rest/public/helpers/get_tickers_prices';
 export { getOpenOrders, getClosedOrders, findClosedOrder, getWsAuthToken } from './api/rest/private/helpers';
